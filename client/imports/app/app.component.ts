fa8516e7b092bc3957e479bab8a6fa77f7a2c496
Zhou Hao <zhou.hao.27@gmail.com>
2016-10-12 16:04:20 +0800

    Using fake data

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -7,6 +7,25 @@ import template from './app.component.html';
   template
 })
 export class AppComponent {
+  parties:any[];
+
   constructor() {
+    this.parties = [
+			{
+        name: 'House warming',
+        description: 'I moved to a new house',
+        location: 'Singapore'
+      },
+			{
+        name: 'Swimming',
+        description: 'At swimming pool',
+        location: 'Shanghai'
+      },
+			{
+        name: 'Rainbow',
+        description: 'I saw rainbow today',
+        location: 'Shenzhen'
+      }      
+    ]
   }
 }
